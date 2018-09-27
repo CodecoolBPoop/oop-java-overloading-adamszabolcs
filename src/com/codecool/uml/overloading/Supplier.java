6717fc1a718ebb97488635f10dc3ee4060ae023d
adamszabolcs <szabolcs.adam91@gmail.com>
2018-09-27 09:47:58 +0200

    Supplier class created, getters-setters implemented, TODOs created

---
+++ src/com/codecool/uml/overloading/Supplier.java
@@ -0,0 +1,46 @@
+package com.codecool.uml.overloading;
+
+import java.util.List;
+
+public class Supplier {
+
+    private int id;
+    private String name;
+    private String description;
+
+    public Supplier() {
+        //TODO
+    }
+
+    public Supplier(String name, String description) {
+        //TODO
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<Product> getProducts() {
+        //TODO
+    }
+
+    public String toString() {
+        //TODO
+    }
+}

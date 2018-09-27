bc322ac7cbe4c2e498611ca528b0b6180aef5878
adamszabolcs <szabolcs.adam91@gmail.com>
2018-09-27 09:51:23 +0200

    FeaturedProductCategory class created, TODOs created

---
+++ src/com/codecool/uml/overloading/FeaturedProductCategory.java
@@ -0,0 +1,17 @@
+package com.codecool.uml.overloading;
+
+import java.util.Date;
+
+public class FeaturedProductCategory extends ProductCategory {
+    private Date startDate;
+    private Date endDate;
+
+    public void schedule(Date startDate, Date endDate) {
+        //TODO
+    }
+
+    @Override
+    public String toString() {
+        //TODO
+    }
+}

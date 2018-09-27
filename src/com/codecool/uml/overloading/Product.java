320fc67a6697a54bef5582536f938bf5f4ac9e7f
adamszabolcs <szabolcs.adam91@gmail.com>
2018-09-27 09:30:52 +0200

    Created methods, implemented some, create TODOs

--- src/com/codecool/uml/overloading/Product.java
+++ src/com/codecool/uml/overloading/Product.java
@@ -1,8 +1,75 @@
 package com.codecool.uml.overloading;
 
+import java.util.Currency;
+import java.util.List;
+import java.util.function.Supplier;
+
 public class Product {
     private int id;
     private String name;
     private float defaultPrice;
+    private Currency defaultCurrency;
+    private ProductCategory productCategory;
+    private Supplier supplier;
+    private List<Product> productList;
 
+    public Product() {
+        //TODO
+    }
+
+    public Product(String name, float defaultPrice, Currency defaultCurrency) {
+        //TODO
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getDefaultPrice() {
+        return defaultPrice;
+    }
+
+    public void setDefaultPrice(float defaultPrice) {
+        this.defaultPrice = defaultPrice;
+    }
+
+    public Currency getDefaultCurrency() {
+        return defaultCurrency;
+    }
+
+    public void setDefaultCurrency(Currency defaultCurrency) {
+        this.defaultCurrency = defaultCurrency;
+    }
+
+    public ProductCategory getProductCategory() {
+        return productCategory;
+    }
+
+    public void setProductCategory(ProductCategory productCategory) {
+        this.productCategory = productCategory;
+    }
+
+    public Supplier getSupplier() {
+        return supplier;
+    }
+
+    public void setSupplier(Supplier supplier) {
+        this.supplier = supplier;
+    }
+
+    public List<Product> getAllProductsBy(ProductCategory productCategory) {
+        //TODO
+    }
+
+    public List<Product> getAllProductsBy(Supplier supplier) {
+        //TODO
+    }
 }
